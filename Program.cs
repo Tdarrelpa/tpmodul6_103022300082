95ab91d08a7ae55258581f318a789cad33000e4b
Tdarrelpa <darrel2208@gmail.com>
2025-03-22 07:29:43 +0700

    Menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using cobaPutarVideo;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract – Timotius Darrel");
+        video1.increasePlayCount(10);
+        video1.printVideoDetails();
+    }
+}
---
+++ cobaPutarVideo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace cobaPutarVideo 
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rnd = new Random(); // Membuat objek random baru untuk menghasilkan angka acak
+            this.id = rnd.Next(00000, 99999); // Buat nomor acak antara 00000 dan 99999 secara otomatis (inlkusif)
+            this.title = title;
+            this.playCount = 0; // Tetapkan playCount ke 0
+        }
+
+        public void increasePlayCount(int playCount)
+        {
+            this.playCount += playCount; // Tambahkan playCount dengan jumlah yang diberikan
+        }
+
+        public void printVideoDetails()
+        {
+            //Console.WriteLine("Video ID: " + this.id); <- Cetak ID video
+            Console.WriteLine($"Video ID: {id}");
+            //Console.WriteLine("Title: " + this.title); <- Cetak judul video
+            Console.WriteLine($"Title: {title}");
+            //Console.WriteLine("Play Count: " + this.playCount); <- Cetak jumlah pemutaran
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
